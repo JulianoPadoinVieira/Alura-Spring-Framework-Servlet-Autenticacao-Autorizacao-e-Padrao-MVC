04bdaa1f4a4b4f6f9465216a3ada355fca2d9577
Juliano Padoin Vieira <proprietyofkurd@hotmail.com>
2021-10-25 22:06:13 -0300

    Parte 3 - Formulário de Login

--- src/main/java/br/com/tresemeia/gerenciador/modelo/Banco.java
+++ src/main/java/br/com/tresemeia/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	//Código estático
@@ -18,7 +19,18 @@ public class Banco {
 		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Alura");
 		lista.add(empresa);
-		lista.add(empresa2);		
+		lista.add(empresa2);	
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("juliano");
+		u1.setSenha("1234");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("kah");
+		u2.setSenha("1234");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -50,6 +62,17 @@ public class Banco {
 				return empresa;				
 			}
 		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+
+		for(Usuario usuario : listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
+		
 		return null;
 	}
 }
